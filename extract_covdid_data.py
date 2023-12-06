46f110b8abdaa2c666074895ec5f7c209bb724e1
Diego Suntaxi <diego.suntaxi@outlook.es>
2023-12-05 22:52:35 -0500

    Add weather API URL and credentials to .env file

--- extract_covdid_data.py
+++ extract_covdid_data.py
@@ -1,20 +1,30 @@
 import requests
 import yaml
+from dotenv import load_dotenv
+import os
 
-url = "https://weatherapi-com.p.rapidapi.com/current.json"
+# load the api url from a .env
+load_dotenv()
+url = os.getenv("WEATHER_API")
+api_key = os.getenv("RAPID_API_KEY")
+api_host = os.getenv("RAPID_API_HOST")
 
+# query params for the api call
 querystring = {"q": "Quito"}
 
+# headers from rapid api
 headers = {
-    "X-RapidAPI-Key": "bcbb788ef0mshb53c3476f943474p16c6f0jsn240dd883d47c",
-    "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+    "X-RapidAPI-Key": api_key,
+    "X-RapidAPI-Host": api_host,
 }
 
+# make the api call
 response = requests.get(url, headers=headers, params=querystring)
 
+# format the data
 data = response.json()
 del data["current"]["condition"]
 formatted_data = {**data["current"], **data["location"]}
 
-print("normal", formatted_data)
+# display the data in a yaml format for a better reading
 print(yaml.dump(formatted_data, allow_unicode=True, default_flow_style=False))
